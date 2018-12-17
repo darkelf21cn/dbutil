6839720074ae07e14fe888dd3a2e2723c589e4ca
Kimi WANG <darkelf21cn@msn.com>
2018-12-17 15:57:29 +0800

    added mysql support

--- mysql.go
+++ mysql.go
@@ -1,20 +1,38 @@
 package dbutil
 
 import (
+	"database/sql"
 	"fmt"
+	"strings"
+
+	//Import MySQL driver
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type Mysql struct {
-	InstanceName string
-	DatabaseName string
-	UserName     string
-	Password     string
+//MySQL contains MySQL connection related properties.
+type MySQL struct {
+	connstr string
 }
 
-func (m *Mysql) TestConnectivity() {
-	fmt.Println("Testing MySQL connectivity.")
+//NewMySQL initializes a new instance of MySQL connection.
+func NewMySQL(Hostname string, Port int, Database, UserName, Password string, Parameters ...string) (db *MySQL) {
+	db = new(MySQL)
+	//[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	connstr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", UserName, Password, Hostname, Port, Database)
+	if len(Parameters) > 0 {
+		parm := ""
+		for _, p := range Parameters {
+			parm += fmt.Sprintf("&%s", p)
+		}
+		parm = strings.TrimLeft(parm, "&")
+		db.connstr = fmt.Sprintf("%s?%s", connstr, parm)
+	} else {
+		db.connstr = connstr
+	}
+	return db
 }
 
-func (m *Mysql) ExecuteSql(Sql string) {
-	fmt.Println("Executing MySQL query.")
+//Open opens a connection to the MySQL.
+func (db *MySQL) open() (conn *sql.DB, err error) {
+	return sql.Open("mysql", db.connstr)
 }
