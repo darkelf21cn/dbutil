886873975d8fca39879d1eb0d8d582b710ec4002
Kimi WANG <darkelf21cn@msn.com>
2018-12-20 13:18:40 +0800

    added bulk function

--- dbutil_test.go
+++ dbutil_test.go
@@ -7,42 +7,72 @@ import (
 )
 
 func Test_Query(t *testing.T) {
-	var db *DBMS
+	ctx := context.Background()
 	var dt *DataTable
 	var err error
-	var ins []string
 
 	//MSSQL
-	mssql := NewMSSQL("NTGSQLMONITOR02", "Nova", "DashboardAcct", "G5agc#FdM$+R", "AutoPilot")
-	db = NewDBMS(context.Background(), 10, 2, mssql)
-	err = db.Open()
+	insMssql := NewMSSQL("NTGSQLMONITOR02", "Nova", "DashboardAcct", "G5agc#FdM$+R", "AutoPilot")
+	mssql := NewDBMS(ctx, 10, 2, insMssql)
+	err = mssql.Open()
 	if err != nil {
 		fmt.Printf("failed to open database:\n%s\n", err.Error())
 		return
 	}
 
-	dt, err = db.Query("SELECT * FROM sys.tables;")
+	dt, err = mssql.Query("SELECT * FROM sys.tables;")
 	if err != nil {
 		fmt.Printf("filed to run the sql query:\n%s\n", err.Error())
 		return
 	}
-	ins, _ = dt.GenerateInsertCommands("test_mssql", 10000)
-	fmt.Printf("%v\n", ins)
+	dt.Print()
 
 	//MySQL
-	mysql := NewMySQL("10.215.70.141", 3306, "nova", "kwang", "k!m!w@ngAP", "interpolateParams=true", "parseTime=true")
-	db = NewDBMS(context.Background(), 10, 2, mysql)
-	err = db.Open()
+	insMysql := NewMySQL("10.215.70.141", 3306, "nova", "kwang", "k!m!w@ngAP", "interpolateParams=true", "parseTime=true")
+	mysql := NewDBMS(context.Background(), 10, 2, insMysql)
+	err = mysql.Open()
 	if err != nil {
 		fmt.Printf("failed to open database:\n%s\n", err.Error())
 		return
 	}
 
-	dt, err = db.Query("SELECT * FROM mysql.user;")
+	dt, err = mysql.Query("SELECT * FROM mysql.user;")
 	if err != nil {
 		fmt.Printf("filed to run the sql query:\n%s\n", err.Error())
 		return
 	}
-	ins, _ = dt.GenerateInsertCommands("test_mysql", 10000)
-	fmt.Printf("%v\n", ins)
+	dt.Print()
+}
+
+func Test_BlukCopy(t *testing.T) {
+	var dt *DataTable
+	var err error
+	ctx := context.Background()
+	insMssql := NewMSSQL("NTGSQLMONITOR02", "MSSQLMonitoringDW", "DashboardAcct", "G5agc#FdM$+R", "AutoPilot")
+	mssql := NewDBMS(ctx, 10, 2, insMssql)
+	err = mssql.Open()
+	if err != nil {
+		fmt.Printf("failed to open database:\n%s\n", err.Error())
+		return
+	}
+
+	insMysql := NewMySQL("10.215.70.141", 3306, "nova", "kwang", "k!m!w@ngAP", "interpolateParams=true", "parseTime=true")
+	mysql := NewDBMS(context.Background(), 10, 2, insMysql)
+	err = mysql.Open()
+	if err != nil {
+		fmt.Printf("failed to open database:\n%s\n", err.Error())
+		return
+	}
+
+	dt, err = mssql.Query("SELECT TOP 100000 * FROM dbo.mConnectivityHist;")
+	if err != nil {
+		fmt.Printf("filed to run the sql query:\n%s\n", err.Error())
+		return
+	}
+	msg, err := mysql.BulkCopy(dt, "TestBcp", 1000, false)
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+	fmt.Printf(msg)
 }
